7714226ffe420f48e1b3dce581de045dab999f1b
suresh950 <s.k.networking175@gmail.com>
2025-03-19 20:54:07 +0530

    paloalto automation change

--- 2_Python_code/paloalto_firewall_lab/initial_script.py
+++ 2_Python_code/paloalto_firewall_lab/initial_script.py
@@ -14,11 +14,14 @@ try:
         api_url = f'https://{ip}/api/?type=op&cmd={xml_command}&key={api_key}'
         urllib3.disable_warnings()
         api_request = requests.get(url=api_url,verify=False)
-        api_responce = api_request.txt
+        api_responce = api_request.text
         xml_tree_root =ET.fromstring(api_responce)
 
+    for a in xml_tree_root.iter():
+        print(a)
+
     for tag in xml_elements:
-        for leaf in xml_tree_root.itrt(tag):
+        for leaf in xml_tree_root.iter(tag):
             if leaf.tag == tag:
                 print(tag+":"+leaf.text)
 except Exception as error:
