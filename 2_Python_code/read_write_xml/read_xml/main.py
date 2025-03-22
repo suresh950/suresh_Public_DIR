07c4b5b1d5cebb80a647b2228778f98eab89b498
suresh950 <s.k.networking175@gmail.com>
2025-03-22 17:59:57 +0530

    commit palo_alto LAB 22/03/2025

--- 2_Python_code/read_write_xml/read_xml/main.py
+++ 2_Python_code/read_write_xml/read_xml/main.py
@@ -5,11 +5,18 @@ root = tree.getroot()
 print(root.tag)
 print(root.attrib)
 
-
+print("==="*50)
 for child in root:
     print(child.tag, child.attrib)
-
+print("==="*50)
 print(root[0][1].text)
-
+print("==="*50)
 for child in root:
-    print(f"rank: {child[0].text} --> year: {child[1].text} --> gdppc")
+    print(f"rank: {child[0].text}"
+          f"--> year: {child[1].text} "
+          f"--> gdppc: {child[2].text} "
+          f"--> neighbor: {child[3].tag} = {child[3].attrib}")
+print("==="*50)
+print(f"{root[0][4].tag} ----> {root[0][4].attrib}")
+print("==="*50)
+print(f"{root[2][4].tag} ----> {root[2][4].attrib}")
