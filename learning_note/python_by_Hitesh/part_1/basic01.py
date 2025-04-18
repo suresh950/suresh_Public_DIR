481de56c781759d1884bf184d3de7db88aadc664
suresh950 <s.k.networking175@gmail.com>
2025-04-18 15:33:15 +0530

    Add basic01 and basic02 Python scripts with initial implementations

---
+++ learning_note/python_by_Hitesh/part_1/basic01.py
@@ -0,0 +1,8 @@
+import os ,sys
+def testbasic01(n):
+    print("Hello World from basic01")
+    print(n)
+    print(os.getcwd())
+    print(sys.platform)
+
+
---
+++ learning_note/python_by_Hitesh/part_1/basic02.py
@@ -0,0 +1,3 @@
+from basic01 import testbasic01
+
+testbasic01("this is test basic02")
